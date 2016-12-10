52fdd0faf5c4398621371f29f0a816f5ced27a27
arthur avezhentsev <celt244@gmail.com>
2016-12-10 22:25:14 +0200

    HW_1

---
+++ HW_1/src/Bender.java
@@ -0,0 +1,11 @@
+public class Bender {
+    public static void main (String[] args){
+
+        int var = 10;
+        String resalt;
+
+       resalt = var % 2 == 0 ? "Указанное число четное" : "Указанное число нечетное";
+
+        System.out.println(resalt);
+    }
+}
